41c209d8a1cf74e5309a63307adfef7f97cdeadb
SaiGanesh <ganeshrejeti1@gmail.com>
2026-05-06 09:48:36 +0530

    Connected frontend with backend

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/messages")
+      .then((res) => res.json())
+      .then((data) => setMessages(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Realtime Chat DevOps Project</h1>
+
+      {messages.map((msg, index) => (
+        <div key={index}>
+          <strong>{msg.user}</strong>: {msg.text}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
